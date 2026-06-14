c2e064d3c36433e7352187c660cea2661f63890c
miguisanson <100124045+miguisanson@users.noreply.github.com>
2026-06-15 07:38:12 +0800

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineSeat
+//
+//  Created by Miguel Sanson on 6/15/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
